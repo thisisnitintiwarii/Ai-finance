0ce18da479d1a2cbadd4f35f8b25bfa3f603bebf
thisisnitintiwarii <thisisnitintiwari@gmail.com>
2025-07-18 13:44:11 +0530

    Fix: explicitly pass Clerk publishableKey in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header.jsx";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
