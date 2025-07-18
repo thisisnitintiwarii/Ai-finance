44b92eb0c85f8be7e7917c5972851ca498032dae
thisisnitintiwarii <thisisnitintiwari@gmail.com>
2025-07-18 13:54:49 +0530

    Fix: add Clerk publishable key and path case issue

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const MainLayout = ({ children }) => {
   return <div className="container mx-auto my-31">{children}</div>;
 };
--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider   publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+>
       <html lang="en">
         <head>
           <link rel="icon" href="/logo-sm.png" sizes="any" />
